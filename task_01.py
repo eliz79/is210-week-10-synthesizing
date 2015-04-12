d43ce27ec918f6bf0c19f6848d0fa57d3ccdf0b7
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-04-12 02:56:20 +0000

    finalizing task_01

--- task_01.py
+++ task_01.py
@@ -2,9 +2,6 @@
 # -*- coding: utf-8 -*-
 """Dictionairies combined for lookup capability."""
 
-import pprint 
-from data import CUSTOMERS, ORDERS
-
 
 def sum_orders(customers, orders):
     """Combining dictionaries for a data function.
@@ -33,39 +30,19 @@ def sum_orders(customers, orders):
             'orders': 1,
             'total': 15}}
     """
-
-    combined_dict = {}    
-    
+    combined_dict = {}
     for order in orders.itervalues():
         cust_id = order['customer_id']
-        t_orders = order['total']
-        if cust_id not in combined_dict:
-            combined_dict[cust_id] = {'orders':1, 'total': t_orders}
-    else:
-        combined_dict[cust_id]['orders']+=1
-        combined_dict[cust_id]['total']+=t_orders
-        pprint.pprint(combined_dict)
-
-    for customer in customers.iterkeys():
-        cust_email = customers.values()
-        cust_key = customers.keys()
-        pprint.pprint(cust_key)
-        cust_email = customers.values()
-        pprint.pprint(cust_email)
-        #if cust_key in customers.keys():
-         #   print 'yes'
-        #pprint.pprint(final)
-         #   combined_dict[customer].update(order)
-            #pprint.pprint(customers)
-
-       # cust_name = combined_dict.keys()
-       # pprint.pprint(cust_name)
-        #cust_email = customer['email': 'email@one.com']
-        #print customer
-      #  if customer in combined_dict:
-       #     combined_dict[cust_id][t_orders] += t_orders
-        #    print customer
-        #else:
-        #    combined_dict[cust_name]
-    
-
+        cust_name = customers[cust_id]['name']
+        cust_email = customers[cust_id]['email']
+        if cust_id not in combined_dict and customers.keys():
+            counter = 1
+            t_orders = order['total']
+        else:
+            combined_dict[cust_id]['orders'] += 1
+            t_orders = ((combined_dict[cust_id]['total']) + (order['total']))
+        final_order = dict(name=cust_name, email=cust_email, orders=counter,
+                           total=t_orders)
+        cust_final_order = {cust_id: final_order}
+        combined_dict.update(cust_final_order)
+    return combined_dict
