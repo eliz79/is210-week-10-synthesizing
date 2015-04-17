997e06b70a48cd7730e5b876835e5fb492bdc88b
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-04-17 00:06:49 +0000

    made changes to task_01.py

--- task_01.py
+++ task_01.py
@@ -2,17 +2,6 @@
 # -*- coding: utf-8 -*-
 """Dictionairies combined for lookup capability."""
 
-ORDERS = {
-            1: {'customer_id': 2, 'total': 10},
-            3: {'customer_id': 2, 'total': 10},
-            4: {'customer_id': 3, 'total': 15}
-         }
-
-CUSTOMERS = {
-            2: {'name': 'person one', 'email': 'email@one.com'},
-            3: {'name': 'person two', 'email': 'email@two.com'}
-            }
-
 
 def sum_orders(customers, orders):
     """Combining dictionaries for a data function.
@@ -47,11 +36,12 @@ def sum_orders(customers, orders):
     """
     combined_dict = {}
     for order in orders.itervalues():
-        if order['customer_id'] in combined_dict:
-            combined_dict[[order['customer_id']]] = {'total': order['total']}
-            combined_dict[[order['customer_id']]]['orders'] = 1
-            combined_dict[order['customer_id']].update(customers[order['cutomer_id']])
+        if order['customer_id'] not in combined_dict:
+            combined_dict[order['customer_id']] = {'total': order['total']}
+            combined_dict[order['customer_id']]['orders'] = 1
+            combined_dict[order['customer_id']].update(customers
+                                                       [order['customer_id']])
         else:
-            combined_dict[[order['customer_id']]]['total'] += order['total']
-            combined_dict[[order['customer_id']]]['orders'] += 1
-    return combined_dict
+            combined_dict[order['customer_id']]['total'] += order['total']
+            combined_dict[order['customer_id']]['orders'] += 1
+            return combined_dict
